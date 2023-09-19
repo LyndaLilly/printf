e6277e70a376353dbd16d9e049477fdf3728c4bf
Samuel Ojedele <samuelojedele2003@gmail.com>
2023-09-19 22:47:04 +0000

    I made some modification!

--- _printf.c
+++ _printf.c
@@ -41,10 +41,10 @@ int _printf(const char *format, ...)
 		{
 			 /* Handle the '%' character followed by conversion specifier */
 			print_buffer(output_buffer, &buf_idx);
-			spec_flags = get_flags(format, &char_idx);
-			spec_width = get_width(format, &char_idx, args_list);
-			spec_prec = get_precision(format, &char_idx, args_list);
-			spec_size = get_size(format, &char_idx);
+			spec_flags = parse_flags(format, &char_idx);
+			spec_width = parse_width(format, &char_idx, args_list);
+			spec_prec = parse_precision(format, &char_idx, args_list);
+			spec_size = parse_size(format, &char_idx);
 			char_idx++;
 			chars_printed = handle_print(format, &char_idx, args_list, output_buffer,
 					 spec_flags, spec_width, spec_prec, spec_size);
---
+++ parse_flags.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * parse_flags - Calculates active flags
+ * @format: Formatted string in which to print the arguments
+ * @i: take a parameter.
+ * Return: Flags:
+ */
+int parse_flags(const char *format, int *i)
+{
+	/* - + 0 # ' ' */
+	/* 1 2 4 8  16 */
+	int j, current_index;
+	int flags = 0;
+	const char VALID_FLAGS[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int FLAG_VALUES[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+
+	for (current_index = *i + 1; format[current_index] != '\0'; current_index++)
+	{
+		for (j = 0; VALID_FLAGS[j] != '\0'; j++)
+		{
+			if (format[current_index] == VALID_FLAGS[j])
+			{
+				flags |= FLAG_VALUES[j];
+				break;
+			}
+		}
+
+		if (VALID_FLAGS[j] == 0)
+			break;
+	}
+
+	*i = current_index - 1;
+
+	return (flags);
+}
